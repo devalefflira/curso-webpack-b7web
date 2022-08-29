65d72d9b1f738abbc9f1e242038f036af11d0f35
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-29 17:14:56 -0300

    Criação de um componente

--- projeto1/src/index.js
+++ projeto1/src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import './css/style.css';
 import AuroraWallpaper from './images/aurora-1197753_1920.jpg';
+import Button from './components/button/button.js';
 
 $(function () {
   $('#button-ptBR').on('click', function () {
@@ -13,4 +14,11 @@ $(function () {
     $('h1').html('Hello World!');
     $(this).addClass('button');
   });
+
+  let button = new Button();
+  button.setTitle('Teste');
+  button.setClick(function () {
+    alert('Clicked!');
+  });
+  $('.area').html(button.render());
 });
